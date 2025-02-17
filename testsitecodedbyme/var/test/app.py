82f0100b05b65f8a07bad7c23428c4b1d5c6a203
GIacomo Lorenzi <giacob500@gmail.com>
2025-02-17 16:43:08 +0000

    v 1.8.2 - Filters based on tags for products work

--- testsitecodedbyme/var/test/app.py
+++ testsitecodedbyme/var/test/app.py
@@ -1,40 +1,20 @@
-"""
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_paginate import Pagination, get_page_args
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+# Available tags
+AVAILABLE_TAGS = ["2", "3", "4"]
 
-# Create some dummy data
-with app.app_context():
-    db.create_all()
-    for i in range(1, 101):
-        db.session.add(Product(name=f"Product {i}"))
-    db.session.commit()
+@app.route('/collections')
+def collections():
+    # Get active tags from URL
+    active_tags = request.args.getlist("active_tag")
 
-@app.route('/')
-def index():
-    # Pagination setup
-    page, per_page, offset = get_page_args()
-    per_page = 10
-    total = Product.query.count()
-    pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
+    # If all tags are selected or no tag is selected, show everything
+    if set(active_tags) == set(AVAILABLE_TAGS) or not active_tags:
+        active_tags = AVAILABLE_TAGS  # Show all by default
 
-    # Query for products for the current page
-    products = Product.query.offset(offset).limit(per_page).all()
-
-    return render_template('index.html', products=products, pagination=pagination)
-
-@app.route('/phone')
-def phone():
-    return render_template('phone_number.html')
+    return render_template("collections.html", active_tags=active_tags, available_tags=AVAILABLE_TAGS)
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
