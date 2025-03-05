2f759c8a405e2d1b01c685c2ed02d9699546670d
GIacomo Lorenzi <giacob500@gmail.com>
2025-03-05 12:16:39 +0000

    v 1.9.5 - Configured automated email

--- testsitecodedbyme/error_handlers.py
+++ testsitecodedbyme/error_handlers.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 def error_handlers(app):
-    @app.errorhandler(404)
+    """ @app.errorhandler(404)
     def invalid_route(e):
         return render_template("error.html", error_message="The source you are looking forward does not exist. Please return to the homepage."), 404
 
@@ -11,4 +11,4 @@ def error_handlers(app):
 
     @app.errorhandler(Exception)
     def handle_all_errors(e):
-        return render_template('error.html', error_message='An unexpected error occurred'), 500
+        return render_template('error.html', error_message='An unexpected error occurred'), 500 """
