dba1b0c3cb8c21c202656b678fdce83bb245957a
GIacomo Lorenzi <giacob500@gmail.com>
2025-03-05 17:09:37 +0000

    v 1.9.6.4 - I try again

---
+++ testsitecodedbyme/error_handlers.py
@@ -0,0 +1,14 @@
+from flask import render_template
+
+def error_handlers(app):
+    @app.errorhandler(404)
+    def invalid_route(e):
+        return render_template("error.html", error_message="The source you are looking forward does not exist. Please return to the homepage."), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template("error.html", error_message="Internal Server Error"), 500
+
+    @app.errorhandler(Exception)
+    def handle_all_errors(e):
+        return render_template('error.html', error_message='An unexpected error occurred'), 500
